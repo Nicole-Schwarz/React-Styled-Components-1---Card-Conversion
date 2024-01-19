9b02ac70ec475922d70004bc8a71409045c7d2ee
Nicole Schwarz <schwarz.nicole82@gmx.de>
2024-01-19 12:05:24 +0100

    change CardTitle

--- components/Card/index.js
+++ components/Card/index.js
@@ -7,7 +7,7 @@ const CardWrapper = styled.div`
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
-const CardTitle = styled.p`
+const CardTitle = styled.h3`
   margin-top: 0;
   margin-bottom: 12px;
   font-size: 1.25rem;
